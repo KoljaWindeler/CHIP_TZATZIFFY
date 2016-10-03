8b8f8d64fd6e394adcd4eb2b3d8db326f4296dfe
KoljaWindeler <Kolja.Windeler@gmail.com>
2016-10-03 13:47:42 +0200

    separated functions

--- projects/flickr/flickr.py
+++ projects/flickr/flickr.py
@@ -19,6 +19,9 @@ def get_api_sig_link(link):
 		signature+=sin_arg[0]
 		signature+=sin_arg[1]
 	url=link+"&api_sig="+hashlib.md5(signature.encode()).hexdigest()
+	return url
+########################################################################################
+def get_page(url):
 	req = urllib.request.urlopen(url)
 	data = req.read().decode('utf-8')
 	#print(data)
@@ -30,18 +33,19 @@ def get_photo_count():
 		return -1
 	else:
 		url="https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+settings.api_key+"&user_id="+settings.user_id+"&per_page=1&page=1&format=json&nojsoncallback=1&auth_token="+settings.token
-		page = get_api_sig_link(url)
+		page = get_page(get_api_sig_link(url))
 		dec=json.loads(page)
 		total=int(dec['photos']['total'])
 	return total
 ########################################################################################
 def get_albums():
 	link="https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key="+settings.api_key+"&user_id="+settings.user_id+"&format=json&nojsoncallback=1&auth_token="+settings.token
-	page = get_api_sig_link(link)
+	page = get_page(get_api_sig_link(link))
 	dec=json.loads(page)
 
 	for i in range(0,int(dec['photosets']['total'])):
 		print("%02d"%i+", id="+dec['photosets']['photoset'][i]['id']+", %04d Phtotos, "%dec['photosets']['photoset'][i]['photos']+dec['photosets']['photoset'][i]['title']['_content'])
+	return dec
 ########################################################################################
 def get_photos_for_album():
 	if(not(settings.photoset_id!="")):
@@ -49,7 +53,7 @@ def get_photos_for_album():
 
 	link="https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&photoset_id="+settings.photoset_id+"&api_key="+settings.api_key+"&user_id="+settings.user_id+"&format=json&nojsoncallback=1&auth_token="+settings.token
 
-	page = get_api_sig_link(link)
+	page = get_page(get_api_sig_link(link))
 	dec=json.loads(page)
 	
 	p_list = []
@@ -71,7 +75,7 @@ def get_random_photo_url(max_photo_count):
 		photo_rand=random.randrange(1,max_photo_count)
 		print("Downloading randomly chosen photo Nr:"+str(photo_rand))
 		url="https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+settings.api_key+"&user_id="+settings.user_id+"&per_page=1&page="+str(photo_rand)+"&format=json&nojsoncallback=1&auth_token="+settings.token
-		page = get_api_sig_link(url)
+		page = get_page(get_api_sig_link(url))
 		dec=json.loads(page)
 		url="https://farm"+str(dec['photos']['photo'][0]['farm'])+".staticflickr.com/"+str(dec['photos']['photo'][0]['server'])+"/"+str(dec['photos']['photo'][0]['id'])+"_"+str(dec['photos']['photo'][0]['secret'])+"_b.jpg"
 		return url
@@ -81,34 +85,4 @@ def get_photo(url):
 		urllib.request.urlretrieve (url, settings.temp_path)
 		print("Done")
 		return settings.temp_path
-########################################################################################
-def setup():
-	if(settings.api_key == "" ):
-		print("you have to get an api key at: https://www.flickr.com/services/apps/create/noncommercial/?")
-		print("add the key and secret values in the setting.py and rerun")
-		exit()
-		return -1
-	elif(settings.frob == "" ):
-		url="https://flickr.com/services/rest/?method=flickr.auth.getFrob&api_key="+settings.api_key+"&format=json"
-		page = get_api_sig_link(url)
-		page=page.replace("jsonFlickrApi(","")[:-1]
-		dec=json.loads(page)
-		settings.frob=dec['frob']['_content']
-		#save_parameter(settings.api_key,settings.secret,settings.frob,settings.token,settings.user_id,settings.temp_path)
-		print("frob generated, please edit your settings.py and set the frob to: "+settings.frob)
-		print("now please open: "+get_api_sig_link("http://flickr.com/services/auth/?api_key="+settings.api_key+"&perms=read&frob="+settings.frob))
-		return -1
-	elif(settings.token == "" or settings.user_id == "" ):
-		url="https://flickr.com/services/rest/?method=flickr.auth.getToken&format=json&api_key="+settings.api_key+"&frob="+settings.frob
-		page = get_api_sig_link(url)
-		page=page.replace("jsonFlickrApi(","")[:-1]
-		dec=json.loads(page)
-		print(dec)
-		settings.token=dec['auth']['token']['_content']
-		settings.user_id=dec['auth']['user']['nsid']
-		print("user_id generated, please edit your settings.py and set the user_id to: "+settings.user_id)
-		print("token generated, please edit your settings.py and set the token to: "+settings.token)
-		return -1
-		#save_parameter(settings.api_key,settings.secret,settings.frob,settings.token,settings.user_id,settings.temp_path)
-	else:
-		print("setup completed, please edit your settings.py and rerun")
+
