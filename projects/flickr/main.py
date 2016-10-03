8c89704e4bdf4fd4d6c14e0b30359f10af86ff4e
KoljaWindeler <Kolja.Windeler@gmail.com>
2016-10-03 15:21:37 +0200

    added splash

--- projects/flickr/main.py
+++ projects/flickr/main.py
@@ -1,18 +1,10 @@
-# basic code from >>
-# http://tkinter.unpythonic.net/wiki/PhotoImage
-# sudo apt-get install python3-pil.imagetk python3-pil python3-tk
-
-
-# extra code -------------------------------------------------------------------------
-from __future__ import print_function
-
 import tkinter as tk
 from tkinter import font
 
 
 from PIL import ImageTk, Image, ImageEnhance
 
-import sys, time, sys, random, platform
+import sys, time, sys, random, platform, time, os
 import flickr
 
 
@@ -50,6 +42,8 @@ class FullScreenApp(object):
 		button.configure(activebackground='black')
 		button.configure(activeforeground='orange')
 		button.configure(bg='black')
+		button.bind("<Button-3>", self.leave)
+
 		# go
 		button.configure(command=lambda: self.update_img(silent = 0))
 		button.pack(anchor=tk.CENTER)
@@ -65,7 +59,7 @@ class FullScreenApp(object):
 		else:
 			print("[init] Switching to album mode")
 
-		self.update_img(silent = 1)
+		self.update_img(silent = 1,splash = 1)
 
 	def update_album_img(self):
 		self.p_list = flickr.get_photos_for_album()
@@ -81,8 +75,9 @@ class FullScreenApp(object):
 		print("[init] %d photos found"%self.max_photo)
 		self.master.after(6*60*60*1000, self.update_total_img)
 
-	def update_img(self,silent = 1):
+	def update_img(self,silent = 1,splash=0):
 		# call it with silent == 0, on button push; else silent == 1 
+		recall_time=5*60*1000
 		print("")
 		print(time.strftime("%H:%M:%S"))
 		if(silent != 1):
@@ -98,15 +93,20 @@ class FullScreenApp(object):
 		else:
 			print("silent update, skipping 'Loading' label")
 
-		# download new image
-		if(len(self.p_list)==0):
-			print("All photos - mode")
-			self.button_image = resize_img(flickr.get_photo(flickr.get_random_photo_url(self.max_photo)))
+		if(splash):
+			print("Loading Splash ...")
+			self.button_image = resize_img(flickr.get_photo("file://"+os.path.dirname(os.path.realpath(__file__))+"/splash.png"))
+			recall_time=1000
 		else:
-			print("Album limited mode")
-			r = random.randrange(0,len(self.p_list))
-			print("selected photo "+str(r)+"/"+str(len(self.p_list)))
-			self.button_image = resize_img(flickr.get_photo(self.p_list[r]))
+			# download new image
+			if(len(self.p_list)==0):
+				print("All photos - mode")
+				self.button_image = resize_img(flickr.get_photo(flickr.get_random_photo_url(self.max_photo)))
+			else:
+				print("Album limited mode")
+				r = random.randrange(0,len(self.p_list))
+				print("selected photo "+str(r)+"/"+str(len(self.p_list)))
+				self.button_image = resize_img(flickr.get_photo(self.p_list[r]))
 			
 			
 		self.button_image_photo = ImageTk.PhotoImage(self.button_image)
@@ -116,9 +116,17 @@ class FullScreenApp(object):
 		# disable callback and restart it
 		if(self.callback_handle is not None):
 			self.master.after_cancel(self.callback_handle)	
-		self.callback_handle = self.master.after(5*60*1000, self.update_img)
+		self.callback_handle = self.master.after(recall_time, self.update_img)
 
 	def leave(self,event):
+		self.button.configure(text = "Exiting ...")
+		# if there was an old image, show it as grayscale
+		if(self.button_image!=""):
+			self.button_image_photo = ImageTk.PhotoImage(self.button_image.convert('LA'))
+			self.button.configure(image = self.button_image_photo)	
+			self.button.image = self.button_image_photo
+			self.master.update()
+			time.sleep(1)
 		exit()
 
 
