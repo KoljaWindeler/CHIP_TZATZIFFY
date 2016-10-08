1bac94053afbf53d988c1b4ea0cc2e2c5d86c630
KoljaWindeler <Kolja.Windeler@gmail.com>
2016-10-08 17:17:55 +0200

    added autorun

--- projects/flickr/prepare.sh
+++ projects/flickr/prepare.sh
@@ -3,6 +3,7 @@ sudo apt update
 sudo apt install python3-pil.imagetk python3-pil python3-tk git -y
 cd /opt/
 sudo git clone https://github.com/KoljaWindeler/CHIP_TZATZIFFY.git
-python3 /opt/CHIP_TZATZIFFY/projects/flickr/setup.py
+sudo python3 /opt/CHIP_TZATZIFFY/projects/flickr/setup.py
+sudo /opt/CHIP_TZATZIFFY/projects/flickr/start.sh
 
 
