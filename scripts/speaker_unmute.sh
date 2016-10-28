ae0f6d3def65dc1aa1eb1f8c7f83caa4484fa97a
Kolja Windeler <KoljaWindeler@gmail.com>
2016-10-28 16:11:03 +0000

    added 4.4 support

--- scripts/speaker_unmute.sh
+++ scripts/speaker_unmute.sh
@@ -1,3 +1,8 @@
-echo 412 > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio412/direction
-echo 1 > /sys/class/gpio/gpio412/value
+LABEL_FILE=`grep -l pcf8574a /sys/class/gpio/*/*label`
+BASE_FILE=`dirname $LABEL_FILE`/base
+BASE=`cat $BASE_FILE`
+let PIN=$BASE+4
+echo $PIN > /sys/class/gpio/export
+D="gpio"$PIN
+echo "out" > /sys/class/gpio/$D/direction
+echo 1 > /sys/class/gpio/$D/value
