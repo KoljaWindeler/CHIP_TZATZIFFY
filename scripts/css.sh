7a7a72aff82352aae5a63b39c57e045bbe2acd6b
Kolja Windeler <Kolja.Windeler@gmail.com>
2016-10-29 20:14:54 +0000

    display switch script

---
+++ scripts/css.sh
@@ -0,0 +1,20 @@
+ #!/bin/bash
+LP=""
+B=`cat /sys/class/backlight/backlight/actual_brightness`
+
+while true; do
+	P=`cat /sys/class/backlight/backlight/bl_power`
+	if [ "$P" != "$LP" ]; then
+		if [ "$P" == "0" ]; then
+			# disp on
+			echo $B > /sys/class/backlight/backlight/brightness
+			echo "switching display on, to level $B"
+		else
+			# disp off
+			echo "switching display off"
+			echo 10 > /sys/class/backlight/backlight/brightness
+		fi
+		LP=$P
+	fi
+  sleep 0.5
+done
