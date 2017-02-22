94b5544cdadf8b421c2748442c1e37c4267bc50a
Kolja Windeler <Kolja.Windeler@gmail.com>
2017-02-22 15:42:09 +0000

    added extended info

--- testitforme.sh
+++ testitforme.sh
@@ -3,7 +3,19 @@ find /lib/firmware/nextthingco/chip/
 echo ""
 
 echo "is module w1_ds2431 loaded"
-lsmod | grep w1_ds2431
+if [[ $(lsmod | grep w1_ds2431) ]]; then
+	echo "yes, module is loaded"
+else
+	echo "module not loaded, reading /etc/modules"
+	if [[ $(cat /etc/modules | grep w1_ds2431) ]]; then
+		echo "it should be loaded, hmmm"
+	else
+		echo "/etc/modules wasn't modified correct"
+		echo "will do it for you now"
+		echo "echo "w1_ds2431" >> /etc/modules" | sudo sh
+		echo "done, please reboot and rerun this"
+	fi
+fi
 echo ""
 
 echo "what eeprom were found"
